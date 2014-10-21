06a04173034e6a20a04f36789bd8ea11621512fa
yohan morales <yohanmorales@hotmail.com>
2014-10-20 22:15:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Footbalo::Application.config.secret_key_base = '6f4d9df0c24fa413c1b05b1cfebfe0b88aed975274603e0e160bb613bdf7fa607d41d48d78c1bd75fcf287a0d7a772e3b75b6cf1e5e85acd915ebad0b40e41d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Footbalo::Application.config.session_store :cookie_store, key: '_footbalo_session'
