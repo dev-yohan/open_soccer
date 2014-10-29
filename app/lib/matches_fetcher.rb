c323ea42d8337ad231eb8912e2c6d3ca6b499da1
yohan morales <yohanmorales@hotmail.com>
2014-10-28 23:48:51 -0500

    result support

--- app/lib/matches_fetcher.rb
+++ app/lib/matches_fetcher.rb
@@ -3,9 +3,7 @@ class MatchesFetcher
   def get_last(page, page_size)
    
     now_date = DateTime.now.in_time_zone(ActiveSupport::TimeZone.new(ENV["timezone"])).to_time.to_i
-    puts "now date"
-    puts now_date
-    matches = Match::Match.where(:match_date => { :$lt =>  now_date }).page(page).per(page_size)
+    matches = Match::Match.where(:match_date => { :$lt =>  now_date }).desc(:match_date).page(page).per(page_size)
 
     return matches
 
