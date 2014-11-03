e8768b7b5b2ae68f01aaa9bc073972fbd6ca34bb
yohan morales <yohanmorales@hotmail.com>
2014-11-03 12:27:12 -0500

    crowd result form

---
+++ app/controllers/matches/crowd_result_controller.rb
@@ -0,0 +1,8 @@
+class Matches::CrowdResultController < ApplicationController
+
+  def new
+
+
+  end  
+
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Footbalo::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'home#index'
 
+   match 'crowd_result/new/:id' => 'matches/crowd_result#new', :as => :crowd_result_new, :via => :get
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
